7b822dd419d07c9ccbc7d84d1c42d2175c90052f
Cheping616 <cheping950605@gmail.com>
2025-08-03 17:20:24 -0400

    i upload the Part.cpp, Part.h, Log.cpp, Log.h

---
+++ SEF Project/Part.h
@@ -0,0 +1,30 @@
+#ifndef PARTS_H
+#define PARTS_H
+#define MAXIMUMLENGTH 51
+#define MAXPARTSIZE 100
+#define MAXLINE 100
+#define LOCATELINE 10
+typedef struct {
+	char PartName[MAXIMUMLENGTH];
+	char PartNumber[MAXIMUMLENGTH];
+	char PartLocate[MAXIMUMLENGTH];
+	float PartCost;
+	int QuantityOnHand;
+	int PartStatus;
+	int PartID;
+}Parts;
+//list all the part information
+void ListallParts(Parts* part, int* size);
+// Get part by ID
+void SearchforPart(Parts* part, int* size);
+// Add a new part
+int AddPart(Parts* part, int* size);
+//update inventory 
+void UpdateInventoryforPart(Parts part[], int* size);
+//save to file
+void SaveToFile(const char* filename, Parts part[], int* size);
+//load from file
+void loadfromfile(const char* filename, Parts part[], int* size);
+#endif
+
+
